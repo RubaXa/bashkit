55121c1e81e7526dffbb7404d0c4932bb5688cd7
RubaXa <ibnRubaXa@gmail.com>
2018-03-16 16:40:10 +0300

    * all

--- all.sh
+++ all.sh
@@ -4,13 +4,13 @@ BASHKIT_DIR="$HOME/.bashkit";
 BASHKIT_VERSION="0.1.0"
 
 source "$BASHKIT_DIR/global.sh";
+source "$BASHKIT_DIR/string.sh";
 source "$BASHKIT_DIR/time.sh";
 source "$BASHKIT_DIR/emoji.sh";
 source "$BASHKIT_DIR/colors.sh";
 source "$BASHKIT_DIR/log.sh";
 source "$BASHKIT_DIR/exec.sh";
 source "$BASHKIT_DIR/git.sh";
-source "$BASHKIT_DIR/string.sh";
 source "$BASHKIT_DIR/risk.sh";
 
 #LOG_LEVEL=$(($LOG_LEVEL | $LOG_LEVEL_VERBOSE));
