a541efd40831d560562c17ad0ab3b1a217a26e70
RubaXa <ibnRubaXa@gmail.com>
2018-07-24 12:54:01 +0300

    * space -> tab

--- string.sh
+++ string.sh
@@ -5,8 +5,8 @@
 stringTest() {
 	if [[ "${@:2}" =~ $1 ]]; then
 		echo $Y;
-    else
-        echo $N;
+	else
+		echo $N;
 	fi
 }
 
@@ -30,39 +30,39 @@ stringGetMatches() {
 stringTrim() {
    local trimmed="$@"
 
-    # Strip leading spaces.
-    while [[ $trimmed == ' '* ]]; do
-       trimmed="${trimmed## }"
-    done
+	# Strip leading spaces.
+	while [[ $trimmed == ' '* ]]; do
+	   trimmed="${trimmed## }"
+	done
 
-    # Strip trailing spaces.
-    while [[ $trimmed == *' ' ]]; do
-        trimmed="${trimmed%% }"
-    done
+	# Strip trailing spaces.
+	while [[ $trimmed == *' ' ]]; do
+		trimmed="${trimmed%% }"
+	done
 
-    echo "$trimmed"
+	echo "$trimmed"
 }
 
 # @param value
 stringLength() {
-    local len=0;
+	local len=0;
 	for a in $@; do
 		v=$(colorRemove $a);
 		len=$((len + ${#v}));
 	done;
-    echo $len
+	echo $len
 }
 
 # @param sep
 # @param text
 stringCutAfter() {
-    local val="${@:2}";
-    echo -e "${val%$1*}";
+	local val="${@:2}";
+	echo -e "${val%$1*}";
 }
 
 # @param sep
 # @param text
 stringCutBefore() {
-    local val="${@:2}";
-    echo -e "${val##*$1}";
+	local val="${@:2}";
+	echo -e "${val##*$1}";
 }
